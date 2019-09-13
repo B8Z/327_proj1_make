73c9fcb633a40d332aaf2eded09bcfad650ea57c
Adam <adam.bates.17@cnu.edu>
2019-09-13 13:47:35 -0400

    fixed a few comments, messed around with myfunc.h a bit

--- myfunc.h
+++ myfunc.h
@@ -1,5 +1,5 @@
-#ifndef MYFUNC_H //Check to make sure that MYFUNC_H is defined
-#define MYFUNC_H
+#ifndef MYFUNC_H //Check to make sure that MYFUNC_H is not defined
+#define MYFUNC_H //Define if it doesn't already exist MYFUNC_H
 
 std::string func(); 
 
